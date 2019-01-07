fe48934c843f67be81df0c5b91701b4db98d1142
Omar Faruk <39029602+omarbangla24@users.noreply.github.com>
2019-01-07 12:47:00 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,45 @@
 $(window).on('load', function(){
+
+    // Nav Hover
     $('ul li a').mouseenter(function(){
         $('li a').removeClass("nav-active");
         $(this).addClass("nav-active");
     });
 
+    // Preloader
     $('#status').fadeOut();
     $('#preloder').delay(350).fadeOut();
+
+ // Smooth Scroll
+    var scrollLink = $('.scroll');
+
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000 );
+    });
+
+    // Active link switching
+    $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+            var sectionOffset = $(this.hash).offset().top - 20;
+
+            if ( sectionOffset <= scrollbarLocation ) {
+                $(this).parent().addClass('nav-active');
+                $(this).parent().siblings().removeClass('nav-active');
+            }
+        })
+
+    });
+
+
+
+
 
 });
 
